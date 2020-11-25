f7cbe3b0c125e9ba512b10a0016291fd699e19df
#bad_sector <64315366+badsector120@users.noreply.github.com>
2020-11-26 02:42:44 +0330

    Add files via upload

---
+++ IndexProjectByRezaChabok.py
@@ -0,0 +1,54 @@
+﻿
+#start project
+def File(a,b,c,p,i):
+    for line in p:
+        line=line.replace("."," ")#bejaye . space.
+        line=line.replace(","," ")#bejaye , space.
+        line=line.lower()#kalmati ke dakhel khat qrar darand ra lowercase mikonad (baraye ngozashtan faeq min lowercase v uppercase.
+        line=line.rstrip()
+        for word in line.split():#kalamat dakhel khat ra mishekanad.
+            if word in c:#agr kalame ie ke behesh residi az qabl dar dictionary c vojod dasht:
+                      b=""+str(i) #list b a yekbar khali va meqdar string shomare khat ra be an ezafe kon.
+                      a=','+b #meqdar shomare khat ra dar a zakhire mikonad.
+                      if b in c[word]:#baraye inke yek kalame momken ast dar yek khat tekrar shode bashad bayad sharti bgozarim ke agar meqdar khat az ghabl dar valiue an kalame boud :
+                            continue#anra darnzr ngir.
+                      else:c[word]=c[word]+a#dargheir in sourat valiue c ra brozresani kon . 
+            else:c[word]=str(i)#baraye dafe aval ke kalame dar dictionary vojod ndarad bayad ebteda anra dar dictionary ezafe konad. 
+            #end of for.
+        if line != "":i+=1#har dafe ke halghe tekrar beshavad yak khat ezafe mishavad.
+        #end of for
+def sortwords(x,y):#sort words indexes in file:
+    flag=False
+    for k ,v in sorted(c.items()):
+     if len(k)>=4:#kalamati ke daraye 4 harf v bishtar hastand chap mishavand.
+      if k[0]==x or k[0]==y:#agar ebtedaye klme ba x ya y shoro shode bod :
+          if flag==False :print(x+":") #harf aval klme ra chap kon.
+          flag = True#baraye inke harf aval klme fqt yek bar chap shavad.
+          print(k,":",v,".")
+while True:
+        try:
+                        print("***************************** ")
+                        print("||What do you want to do?  || ")
+                        print("||*************************||  \n||1.show index of file.    ||\n||2.Exit.                  ||\n*****************************")
+                        n =(input("Please Enter the number of button:"))
+                        if n == "2":
+                                print("---------------------------------------------------------------------------------------------------\n°•○●•°•○●---Made by Reza Chabok Shahroudi---●○•°•●○•°")
+                                break
+                        if n == "1":
+                                print("---------------------------------------------------------------------------------------------------\n")
+                                address=str(input("Enter The Address Of Your File With .txt(C:\\filename.txt):"))
+                                p=open(address)#file ra mikhanad.
+                                a=list()#list a
+                                b=list()#list b
+                                c=dict()#dictionary c
+                                i=1#avalin khat meqdarash braber ba1 ast.
+                                File(a,b,c,p,i)
+                        #morattab karadn word ha bar asas horof alefba(english uppercase):
+                                print("INDEX OF WORDS IN THIS FILE:")
+                                for i in range(26):
+                                    sortwords(chr(i+65),chr(i+65+32))
+                                print("----------------------------------------------------------------------------")
+                        else:print("This button not exist!!!\n")
+        except IOError as e:
+                print("*********************************************************************\n*Something bad as %s happened!*\n*********************************************************************"%e)
+        #end project.
---
+++ Readme.txt
@@ -0,0 +1,18 @@
+HI.
+Please first copy all the text code and paste in to pycharm or other IDE and run it in this IDE.
+then you should to enter a number of two button : 1 & 2.enter 1 for find your file inedxes and enter 2 for exit.
+when you entered button 1 prgrame  wants you enter the address of your text file.
+then you enterd address of a text file , programe will show indexes of your text file.
+this program have 2 function :1.File , 2.sortwords
+File function :
+in File function first read lines of your text file and then replace all . and , character with space for split them
+and then  convert all words to lower case and then 
+then remove the \n in the end of line.
+After making the necessary changes , this function split the words in line and Checks for all the words to see if the word already exists in the c dictionary. If there is, it adds the line number to the valiue of the dictionary c ,Otherwise, if it doesn't exist, create a key with the same word and set its valiue value to the line number.
+Sortwords function:
+This function first places the flag incorrectly and receives two characters.
+ Search for them in dictionary items c. If the first letter of the word is equal to one of the received characters, first if the flag value is incorrect, print the uppercase letter first, then set the flag value equal to the correct one, 
+then a group of words 4 It prints letters or more than 4 letters and they start with that capital letter.
+This function is called by a loop. In that loop, each time it is repeated, one of the English letters is sent to the function in alphabetical order, both large and small.
+Reza Chabok Shahroudi.
+Email:rezahoseinchabok@gmail.com
